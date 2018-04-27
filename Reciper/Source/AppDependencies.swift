d9279d968fcc5972aff664ac63792d8fe266598d
Aleksandra Kwiecien <aleksandra.kwiecien@altconnect.pl>
2018-04-27 14:08:24 +0200

    Project structure.

---
+++ Reciper/Source/AppDependencies.swift
@@ -0,0 +1,14 @@
+//
+//  AppDependencies.swift
+//  Reciper
+//
+//  Created by Aleksandra Kwiecień on 27.04.2018.
+//  Copyright © 2018 Test. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class AppDependencies {
+    
+}
