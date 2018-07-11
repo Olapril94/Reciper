7a16e48876bcf870737fe3888aa4428103ea0495
Aleksandra Kwiecien <olapril94@gmail.com>
2018-07-11 14:38:18 -0400

    Fonts, UIColor structure, Login view.

---
+++ Reciper/Source/Config/Color.swift
@@ -0,0 +1,25 @@
+//
+//  Color.swift
+//  Reciper
+//
+//  Created by Aleksandra Kwiecien on 11/07/2018.
+//  Copyright © 2018 Aleksandra Kwiecien. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct Color {
+    
+    // MARK: - Green
+    
+    static let mainGreen = UIColor(hexString: "#848D48")
+    
+    // MARK: - Gray
+    
+    static let darkGray = UIColor(hexString: "#525252")
+    
+    // MARK: - Pink
+    
+    static let lightPink  = UIColor(hexString: "#f5d9e3")
+}
---
+++ Reciper/Source/Login/viewModel/LoginViewModel.swift
@@ -0,0 +1,9 @@
+//
+//  LoginViewModel.swift
+//  Reciper
+//
+//  Created by Aleksandra Kwiecien on 11/07/2018.
+//  Copyright © 2018 Aleksandra Kwiecien. All rights reserved.
+//
+
+import Foundation
